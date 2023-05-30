8565adda8674eaf52899c8e7c9c330adca1b3207
Fardin2112 <fardinalam2112@gmail.com>
2023-05-30 22:04:09 +0530

    completed

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-import { add } from "../functions.js"
-
-// Challenge: Make sure that add is working by console logging add with two number arguments.
-console.log(add(1,1))
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL: "https://playground-c5b18-default-rtdb.europe-west1.firebasedatabase.app"
+    databaseURL: "https://shopping-list-2666f-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database, "movies")
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
-    let inputValue = inputFieldEl.value
-    
+    let inputValue = inputFieldEl.value   
+    push(moviesInDB, inputValue)   
     console.log(`${inputValue} added to database`)
+    inputFieldEl.value = ""
 })
+
+
+// line 4  ->  here we connecting firebase databaseURL
+// line 11 ->  this is used to make a file to save all those data
+//             movie is file name
+// line 19 ->  here we pushing the of input value in firebase
